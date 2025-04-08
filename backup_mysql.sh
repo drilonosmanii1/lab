4f3188b49f436e79a86884b02f47fb412a5d406c
unknown <osmanidriloon@gmail.com>
2025-04-08 15:20:20 +0200

    stuff

---
+++ backup_mysql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+USER="root"             
+PASSWORD="rootpassword" 
+DATABASE="my_database"  
+BACKUP_DIR="$HOME/backups"  
+
+
+DATE=$(date +"%Y%m%d%H%M")
+BACKUP_FILE="$BACKUP_DIR/$DATABASE-backup-$DATE.sql"
+
+
+mkdir -p $BACKUP_DIR
+
+
+docker exec mysql mysqldump -u $USER -p$PASSWORD $DATABASE > $BACKUP_FILE
+
+
+if [ $? -eq 0 ]; then
+    echo "Backup successfully created at $BACKUP_FILE"
+else
+    echo "Error: Backup failed"
+fi
